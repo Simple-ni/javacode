7d096ddb2d547aacee809fd780f41e07055b7dac
Simple <2940318216@qq.com>
2025-11-03 20:38:20 +0800

    second save.first

---
+++ reflectionbank/test.java
@@ -0,0 +1,26 @@
+package reflectionbank;
+
+import java.lang.reflect.Method;
+
+public class test {
+    public static void main(String[] args) {
+        try {
+         
+            Class<?> clazz = Class.forName("bank.BankAccount");
+           
+            Object instance = clazz.getDeclaredConstructor().newInstance();
+          
+            Method method = clazz.getMethod("deposit", double.class);
+    
+            method.invoke(instance, 1000.0);
+        
+            Method withdrawMethod = clazz.getMethod("withdraw", double.class);
+            withdrawMethod.invoke(instance, 500.0);
+        
+            Method checkBalanceMethod = clazz.getMethod("checkBalance");
+            checkBalanceMethod.invoke(instance);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
