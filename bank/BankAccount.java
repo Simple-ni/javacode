a02b46c46ef4a5867077c7df09665dbea89c7dde
Simple <2940318216@qq.com>
2025-11-03 19:58:43 +0800

    second save

--- bank/BankAccount.java
+++ bank/BankAccount.java
@@ -1,28 +1,39 @@
 package bank;
 
 public class BankAccount {
-    private double balance;
-   public BankAccount(){
+    private double balance; // 账户余额
 
-   }
-   public BankAccount(double balance){
-     this.balance = balance < 0 ? 0 : balance;
-
-   }
-   public void deposit(double amount) {
-        if (amount > 0) balance += amount;
+    // 构造方法，初始化余额为0
+    public BankAccount() {
+        this.balance = 0.0;
     }
 
-    public boolean withdraw(double amount) {
-        if (amount > 0 && amount <= balance) {
-            balance -= amount;
-            return true;
+    // 存款方法
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+            System.out.println("存入金额：" + amount + "，当前余额：" + balance);
+        } else {
+            System.out.println("存款金额必须大于0");
         }
-        return false;
     }
 
-    public double getBalance() {
-        return balance;
+    // 取款方法
+    public void withdraw(double amount) {
+        if (amount > 0) {
+            if (amount <= balance) {
+                balance -= amount;
+                System.out.println("取出金额：" + amount + "，当前余额：" + balance);
+            } else {
+                System.out.println("余额不足，无法取款");
+            }
+        } else {
+            System.out.println("取款金额必须大于0");
+        }
     }
 
+    // 查看余额方法
+    public void checkBalance() {
+        System.out.println("当前余额：" + balance);
+    }
 }
